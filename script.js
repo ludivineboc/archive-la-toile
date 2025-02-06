37b8493d39c23e9992eea48cd670344ba4f48e1c
Ludivine Bocquier <98520169+ludivineboc@users.noreply.github.com>
2025-02-06 20:49:48 +0100

    setup

---
+++ script.js
@@ -0,0 +1,68 @@
+// Effectue une requête HTTP GET vers le fichier local "data.json" pour récupérer une liste de produits
+fetch("data.json")
+  .then((rep) => {
+    if (!rep.ok) {
+      throw new Error(`Erreur de chargement: ${rep.status} ${rep.statusText}`);
+    }
+    return rep.json();
+  })
+  .then((donnee) => {
+    afficher(donnee);
+  })
+  .catch((error) => {
+    console.error("Une erreur est survenue :", error);
+  });
+
+function afficher(data) {
+  // Récupérer N° promo dans l'url
+  const match = window.location.href.match(/\/([^\/]+)\.html$/);
+  const Npromo = match ? match[1] : null;
+
+  const paris = data[Npromo].Paris;
+  const lyon = data[Npromo].Lyon;
+  const ste = data[Npromo].St_Etienne;
+
+  let item = ``;
+  let listProjectParis = document.querySelector(`#parislist`);
+  let listProjectLyon = document.querySelector(`#lyonlist`);
+  let listProjectSte = document.querySelector(`#stelist`);
+
+  /* Liste lyon */
+  lyon.forEach((projet) => {
+    let titre = projet.titre;
+    let lien = projet.lien;
+
+    item += `
+            <li><a href="${lien}"> ${titre} </a></li>
+            `;
+  });
+
+  listProjectLyon.innerHTML += item;
+  item = ``;
+
+  /* Liste Paris */
+  paris.forEach((projet) => {
+    let titre = projet.titre;
+    let lien = projet.lien;
+
+    item += `
+        <li><a href="${lien}"> ${titre} </a></li>
+        `;
+  });
+
+  listProjectParis.innerHTML += item;
+  item = ``;
+
+  /* Liste Ste */
+  ste.forEach((projet) => {
+    let titre = projet.titre;
+    let lien = projet.lien;
+
+    item += `
+            <li><a href="${lien}"> ${titre} </a></li>
+            `;
+  });
+
+  listProjectSte.innerHTML += item;
+  item = ``;
+}
